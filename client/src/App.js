89d6190d39b4faa458ac47877c9f111db316ffd6
Avik Dey <abhik.dey.1992@gmail.com>
2021-06-10 22:33:53 +0530

    Gettting an error

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import SimpleBottomNavigation from "./components/MainNav";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-    <div className="app">
-        Hello
-    </div>
-    <SimpleBottomNavigation />
-    </>
+      <div className="app">
+        <Container>
+          Hey
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
