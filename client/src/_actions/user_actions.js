0210181bef95abb799b198db832bfbb96220205c
Avik Dey <abhik.dey.1992@gmail.com>
2021-06-16 19:29:26 +0530

    Added Files

---
+++ client/src/_actions/user_actions.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import {
+  LOGIN_USER,
+  REGISTER_USER,
+  // AUTH_USER,
+  LOGOUT_USER,
+} from "./types";
+
+export function registerUser() 
---
+++ client/src/_reducers/index.js
@@ -0,0 +1,5 @@
+import user from './user_reducer';
+
+
+
+export default rootReducer;
